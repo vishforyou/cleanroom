67cca61f0da277e0e950e672246adc61c6d06726
vishforyou <39515086+vishforyou@users.noreply.github.com>
2023-04-23 23:48:17 +0530

    Add files via upload

--- deleteuser.sh
+++ deleteuser.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-
+hname=$(hostname)
 grep cleanroom /etc/passwd| cut -d ":" -f1| while read line
 do
 userdel -rf $line
 done
-
 echo "user/s deleted"
+
+
+##
+# Check if user got deleted
+# then create a new user
+grep ^cleanroom /etc/passwd
+retval=$(echo $?)
+if [ $retval -eq 1 ];then
+useradd -d /home/${hname} -m ${hname}
+echo "${hname}:${hname}@arw" | chpasswd
+fi
+
