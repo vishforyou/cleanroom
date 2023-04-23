fc094dffc7667ebd2f2ac1109bb2eed5ba91b9f0
vishforyou <39515086+vishforyou@users.noreply.github.com>
2023-04-23 23:17:10 +0530

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+hname=$(hostname)
+## delete user
 cp deleteuser.sh /root
 chmod +x /root/deleteuser.sh
 cp run-before-shutdown.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable run-before-shutdown.service
+
+# Check if user got deleted
+# then create a new user
+grep ^cleanroom /etc/passwd  
+retval=$(echo $?)
+if [ $retval -eq 1 ];then
+useradd -d /home/${hname} -m ${hname}
+echo "${hname}@arw" | chpasswd 
+fi
+
+
+exit 0
+
+
+
