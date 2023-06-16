bf21f812a031dac946453ea47a88653b661a8f65
vishforyou <39515086+vishforyou@users.noreply.github.com>
2023-06-17 00:19:58 +0530

    Update main_wrkspace.sh

--- main_wrkspace.sh
+++ main_wrkspace.sh
@@ -11,15 +11,18 @@ systemctl enable run-before-shutdown-wrkspaces.service
 
 curl https://intoli.com/install-google-chrome.sh | bash
 
-python3 -m pip3 install numpy opencv-python pillow labelImg PyMuPDF tqdm
+pip3 install --upgrade setuptools
+pip3 install --upgrade pip
+python3 -m pip install numpy opencv-python pillow labelImg PyMuPDF tqdm
 cr_dir=$(ls -1 /volumes/user/home| grep -v 'lost+found')
 pth="/volumes/user/home/${cr_dir}"
-cp convert_pdf_to_image.py /home/{pth}/bin
-mkdir -p /home/{pth}/input_dir
-chmod 777 /home/{pth}/input_dir
-mkdir -p /home/{pth}/output_dir
-chmod 777 /home/{pth}/output_dir
-cp README_for_conversion.txt $pth
+cp convert_pdf_to_image.py /home/${cr_dir}
+cp convert_pdf_to_image.py /home/${cr_dir}/bin
+mkdir -p /home/${cr_dir}/input_dir
+chmod 777 /home/${cr_dir}/input_dir
+mkdir -p /home/${cr_dir}/output_dir
+chmod 777 /home/${cr_dir}/output_dir
+cp README_for_conversion.txt /home/${cr_dir}
 
 
 
