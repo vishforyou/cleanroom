1e52907bc0f3be508332835218c57077f3dcbacc
vishforyou <39515086+vishforyou@users.noreply.github.com>
2023-06-16 22:49:51 +0530

    Update main_wrkspace.sh

--- main_wrkspace.sh
+++ main_wrkspace.sh
@@ -12,4 +12,6 @@ systemctl enable run-before-shutdown-wrkspaces.service
 curl https://intoli.com/install-google-chrome.sh | bash
 
 python3 -m pip3 install numpy opencv-python pillow labelImg PyMuPDF tqdm
+cp convert_pdf_to_image.py $HOME
+
 
