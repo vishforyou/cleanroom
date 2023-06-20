68c1727ee859045cbe2150367301248efb3194fe
vishforyou <39515086+vishforyou@users.noreply.github.com>
2023-06-20 12:02:27 +0530

    Update main_wrkspace.sh

--- main_wrkspace.sh
+++ main_wrkspace.sh
@@ -9,6 +9,7 @@ chmod +x /root/deletedir.sh
 systemctl daemon-reload
 systemctl enable run-before-shutdown-wrkspaces.service
 curl https://intoli.com/install-google-chrome.sh | bash
+yum install xcb-util* -y
 #yum install google-chrome --nogpgcheck -y
 pip3 install --upgrade setuptools
 pip3 install --upgrade pip
@@ -17,11 +18,12 @@ cr_dir=$(ls -1 /volumes/user/home| grep -v 'lost+found')
 pth="/volumes/user/home/${cr_dir}"
 cp convert_pdf_to_image.py /home/${cr_dir}
 cp convert_pdf_to_image.py /home/${cr_dir}/bin
-mkdir -p /home/${cr_dir}/input_dir
-chmod 777 /home/${cr_dir}/input_dir
-mkdir -p /home/${cr_dir}/output_dir
-chmod 777 /home/${cr_dir}/output_dir
+mkdir -p /home/${cr_dir}/source
+chmod 777 /home/${cr_dir}/target
+mkdir -p /home/${cr_dir}/source
+chmod 777 /home/${cr_dir}/target
 cp README_for_conversion.txt /home/${cr_dir}
+cp classes.txt /home/${cr_dir}
 
 
 
