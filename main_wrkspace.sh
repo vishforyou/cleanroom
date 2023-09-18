5f686530032096e183a539083868e4d806cd55df
vishforyou <39515086+vishforyou@users.noreply.github.com>
2023-09-18 15:00:24 +0530

    Update main_wrkspace.sh

--- main_wrkspace.sh
+++ main_wrkspace.sh
@@ -3,7 +3,7 @@
 cp hosts /etc/
 cp deletedir.sh /root
 cp run-before-shutdown-wrkspaces.service /etc/systemd/system/
-cp hostcron /etc/cron.d/hostcron
+cp hostcron_wrkspace /etc/cron.d/hostcron_wrkspace
 # Setup for delete and creation
 chmod +x /root/deletedir.sh
 systemctl daemon-reload
